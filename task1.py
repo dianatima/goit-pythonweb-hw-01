afd5b04c94b67416ac7b6f75384fe6dc1055fdf8
Yulia <nayanzina@gmail.com>
2025-02-27 01:42:14 +0200

    hw-01

--- task1.py
+++ task1.py
@@ -1,44 +1,44 @@
 from abc import ABC, abstractmethod
 
 class Vehicle(ABC):
-    def __init__(self, make, model, region):
+    def __init__(self, make: str, model: str, region: str):
         self.make = make
         self.model = model
         self.region = region
 
     @abstractmethod
-    def start_engine(self):
+    def start_engine(self) -> None:
         pass
 
 class Car(Vehicle):
-    def start_engine(self):
+    def start_engine(self) -> None:
         print(f"{self.make} {self.model} {self.region}: Двигун запущено")
 
 class Motorcycle(Vehicle):
-    def start_engine(self):
+    def start_engine(self) -> None:
         print(f"{self.make} {self.model} {self.region}: Мотор заведено")
 
 class VehicleFactory(ABC):
     @abstractmethod
-    def create_car(self, make, model):
+    def create_car(self, make: str, model: str) -> Car:
         pass
     
     @abstractmethod
-    def create_motorcycle(self, make, model):
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
         pass
 
 class USVehicleFactory(VehicleFactory):
-    def create_car(self, make, model):
+    def create_car(self, make: str, model: str) -> Car:
         return Car(make, model, "US Spec")
     
-    def create_motorcycle(self, make, model):
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
         return Motorcycle(make, model, "US Spec")
 
 class EUVehicleFactory(VehicleFactory):
-    def create_car(self, make, model):
+    def create_car(self, make: str, model: str) -> Car:
         return Car(make, model, "EU Spec")
     
-    def create_motorcycle(self, make, model):
+    def create_motorcycle(self, make: str, model: str) -> Motorcycle:
         return Motorcycle(make, model, "EU Spec")
 
 
