9577bbc202ffa67ab82e1ebc61938177369bd78b
Yulia <nayanzina@gmail.com>
2025-02-27 01:37:32 +0200

    hw-01

--- task2.py
+++ task2.py
@@ -1,35 +1,39 @@
 from abc import ABC, abstractmethod
+from typing import List
 
 class Book(ABC):
-    def __init__(self, title, author, year):
+    def __init__(self, title: str, author: str, year: str):
         self.title = title
         self.author = author
         self.year = year
 
+    def __str__(self):
+        return f"Title: {self.title}, Author: {self.author}, Year: {self.year}"
+
 class LibraryInterface(ABC):
     @abstractmethod
-    def add_book(self, book: Book):
+    def add_book(self, book: Book) -> None:
         pass
 
     @abstractmethod
-    def remove_book(self, title):
+    def remove_book(self, title: str) -> None:
         pass
 
     @abstractmethod
-    def show_books(self):
+    def show_books(self) -> None:
         pass
 
 class Library(LibraryInterface):
     def __init__(self):
         self.books = []
 
-    def add_book(self, book: Book):
+    def add_book(self, book: Book) -> None:
         self.books.append(book)
 
-    def remove_book(self, title):
+    def remove_book(self, title: str) -> None:
         self.books = [book for book in self.books if book.title != title]
 
-    def show_books(self):
+    def show_books(self) -> None:
         for book in self.books:
             print(book)
 
@@ -37,14 +41,14 @@ class LibraryManager:
     def __init__(self, library: LibraryInterface):
         self.library = library
 
-    def add_book(self, title, author, year):
+    def add_book(self, title: str, author: str, year: str) -> None:
         book = Book(title, author, year)
         self.library.add_book(book)
 
-    def remove_book(self, title):
+    def remove_book(self, title: str) -> None:
         self.library.remove_book(title)
 
-    def show_books(self):
+    def show_books(self) -> None:
          self.library.show_books()
 
 
